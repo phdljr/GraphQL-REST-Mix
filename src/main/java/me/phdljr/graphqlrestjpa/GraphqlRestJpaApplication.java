cb1648119b3110c73f316bdc349f7a246495e5e2
phdljr <phdljr@naver.com>
2024-08-03 01:11:46 +0900

    init

---
+++ src/main/java/me/phdljr/graphqlrestjpa/GraphqlRestJpaApplication.java
@@ -0,0 +1,13 @@
+package me.phdljr.graphqlrestjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlRestJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlRestJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/phdljr/graphqlrestjpa/GraphqlRestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.phdljr.graphqlrestjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlRestJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
